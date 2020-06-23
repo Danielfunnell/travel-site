bbfe3a44b2ed7688ddb01e1cc1cfb37f5cac7a15
Daniel <danielfunnell@yahoo.co.uk>
2020-06-23 16:12:28 +0100

    Completed desktop and mobile header.

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,5 +1,12 @@
 import '../styles/styles.css'
+import './modules/MobileMenu'
+import MobileMenu from './modules/MobileMenu'
+
+let mobileMenu = new MobileMenu();
 
 if (module.hot) {
     module.hot.accept()
 }
+
+
+
